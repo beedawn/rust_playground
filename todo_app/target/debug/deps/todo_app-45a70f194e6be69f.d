c74b0ef2a8bb9f293d8c816d26ebe8f1050bd7cc
Bee <bee@bees-mbp-2.lan>
2023-12-27 20:18:43 -0500

    updates

--- todo_app/target/debug/deps/todo_app-45a70f194e6be69f.d
+++ todo_app/target/debug/deps/todo_app-45a70f194e6be69f.d
@@ -1,6 +1,18 @@
-/Users/bee/GitHub/rust_playground/todo_app/target/debug/deps/todo_app-45a70f194e6be69f: src/main.rs src/state.rs
+/Users/bee/GitHub/rust_playground/todo_app/target/debug/deps/todo_app-45a70f194e6be69f: src/main.rs src/state.rs src/to_do/mod.rs src/to_do/structs/mod.rs src/to_do/structs/base.rs src/to_do/structs/done.rs src/to_do/structs/pending.rs src/to_do/enums.rs src/to_do/traits/mod.rs src/to_do/traits/create.rs src/to_do/traits/delete.rs src/to_do/traits/edit.rs src/to_do/traits/get.rs src/processes.rs
 
-/Users/bee/GitHub/rust_playground/todo_app/target/debug/deps/todo_app-45a70f194e6be69f.d: src/main.rs src/state.rs
+/Users/bee/GitHub/rust_playground/todo_app/target/debug/deps/todo_app-45a70f194e6be69f.d: src/main.rs src/state.rs src/to_do/mod.rs src/to_do/structs/mod.rs src/to_do/structs/base.rs src/to_do/structs/done.rs src/to_do/structs/pending.rs src/to_do/enums.rs src/to_do/traits/mod.rs src/to_do/traits/create.rs src/to_do/traits/delete.rs src/to_do/traits/edit.rs src/to_do/traits/get.rs src/processes.rs
 
 src/main.rs:
 src/state.rs:
+src/to_do/mod.rs:
+src/to_do/structs/mod.rs:
+src/to_do/structs/base.rs:
+src/to_do/structs/done.rs:
+src/to_do/structs/pending.rs:
+src/to_do/enums.rs:
+src/to_do/traits/mod.rs:
+src/to_do/traits/create.rs:
+src/to_do/traits/delete.rs:
+src/to_do/traits/edit.rs:
+src/to_do/traits/get.rs:
+src/processes.rs:
