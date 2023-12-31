7be3b3a8dcfeef4b574984c327735679b33696ea
Bee <bee@bees-mbp-2.lan>
2023-12-31 06:21:30 -0500

    updates

--- web_app/target/debug/deps/web_app-864d9df6e3708c68.d
+++ web_app/target/debug/deps/web_app-864d9df6e3708c68.d
@@ -1,5 +1,9 @@
-/Users/bee/GitHub/rust_playground/web_app/target/debug/deps/web_app-864d9df6e3708c68: src/main.rs
+/Users/bee/GitHub/rust_playground/web_app/target/debug/deps/web_app-864d9df6e3708c68: src/main.rs src/views/mod.rs src/views/auth/mod.rs src/views/auth/login.rs src/views/auth/logout.rs
 
-/Users/bee/GitHub/rust_playground/web_app/target/debug/deps/web_app-864d9df6e3708c68.d: src/main.rs
+/Users/bee/GitHub/rust_playground/web_app/target/debug/deps/web_app-864d9df6e3708c68.d: src/main.rs src/views/mod.rs src/views/auth/mod.rs src/views/auth/login.rs src/views/auth/logout.rs
 
 src/main.rs:
+src/views/mod.rs:
+src/views/auth/mod.rs:
+src/views/auth/login.rs:
+src/views/auth/logout.rs:
--- web_app/target/debug/web_app.d
+++ web_app/target/debug/web_app.d
@@ -1,1 +1,1 @@
-/Users/bee/GitHub/rust_playground/web_app/target/debug/web_app: /Users/bee/GitHub/rust_playground/web_app/src/main.rs
+/Users/bee/GitHub/rust_playground/web_app/target/debug/web_app: /Users/bee/GitHub/rust_playground/web_app/src/main.rs /Users/bee/GitHub/rust_playground/web_app/src/views/auth/login.rs /Users/bee/GitHub/rust_playground/web_app/src/views/auth/logout.rs /Users/bee/GitHub/rust_playground/web_app/src/views/auth/mod.rs /Users/bee/GitHub/rust_playground/web_app/src/views/mod.rs
