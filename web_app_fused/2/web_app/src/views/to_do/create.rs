0973cae5b1d7c42b8e054d728f166851af3a61d2
Bee <bee@bees-mbp-2.lan>
2024-01-02 06:31:54 -0500

    updates

--- web_app_fused/2/web_app/src/views/to_do/create.rs
+++ web_app_fused/2/web_app/src/views/to_do/create.rs
@@ -1,13 +1,15 @@
+use actix_web::HttpResponse;
 use serde_json::value::Value;
 use serde_json::Map;
 use actix_web::HttpRequest;
 
 use crate::to_do::{to_do_factory, enums::TaskStatus};
+use crate::json_serialization::to_do_items::ToDoItems;
 use crate::state::read_file;
 use crate::processes::process_input;
 
 
-pub async fn create(req: HttpRequest) -> String {
+pub async fn create(req: HttpRequest) -> HttpResponse {
     let state: Map<String, Value> = read_file(
         "./state.json"); // step 1
 
@@ -17,5 +19,5 @@ pub async fn create(req: HttpRequest) -> String {
     let item = to_do_factory(&title.as_str(),
                              TaskStatus::PENDING); // step 3
     process_input(item, "create".to_string(), &state); // step 4
-    return format!("{} created", title) // step 5
+    return HttpResponse::Ok().json(ToDoItems::get_state())
 }
