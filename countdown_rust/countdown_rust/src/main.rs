403ef0c1469ffec66ca8deedcbbdf234e5fe3de5
Bee <bee@bees-mbp-2.lan>
2024-01-24 07:08:31 -0500

    updates

--- countdown_rust/countdown_rust/src/main.rs
+++ countdown_rust/countdown_rust/src/main.rs
@@ -8,7 +8,7 @@ use std::ops::Sub;
 fn main() -> Result<(), eframe::Error> {
     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
     let options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
+        viewport: egui::ViewportBuilder::default().with_inner_size([360.0, 200.0]),
         ..Default::default()
     };
     eframe::run_native(
@@ -26,6 +26,7 @@ fn main() -> Result<(), eframe::Error> {
 struct MyApp {
     now_time: NaiveDate,
     deadline: NaiveDate,
+    countdown: i64,
 }
 
 impl Default for MyApp {
@@ -33,6 +34,7 @@ impl Default for MyApp {
         Self {
             now_time: NaiveDate::from_ymd_opt(OffsetDateTime::now_utc().year(),OffsetDateTime::now_utc().month() as u32,OffsetDateTime::now_utc().day() as u32).expect("today date error"),
             deadline: NaiveDate::from_ymd_opt(2024,1,15).expect("error"),
+            countdown: 0,
 
 
         }
@@ -42,20 +44,81 @@ impl Default for MyApp {
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("countdown");
+            //ui.heading("countdown");
             ui.horizontal(|ui| {
-                let name_label = ui.label("Your name: ");
+             //   let name_label = ui.label("Your name: ");
               //  ui.text_edit_singleline(&mut self.now_time)
               //       .labelled_by(name_label.id);
             });
-            ui.label(format!("Today's date is '{}'", self.now_time));
+           // ui.label(format!("Today's date is '{}'", self.now_time));
             //ui.add(egui_extras::DatePickerButton::new(&mut self.now_time));
-            ui.add(egui_extras::DatePickerButton::new(&mut self.deadline));
+           // ui.add(egui_extras::DatePickerButton::new(&mut self.deadline));
+            if ui.add(egui_extras::DatePickerButton::new(&mut self.deadline).arrows(false)).clicked_elsewhere(){
+                println!("calendar clicker!");
 
-            ui.label(format!("Hello '{}', age {}", self.now_time, self.deadline));
-            if ui.button("Test").clicked(){
-                println!("hello!");
-            };
+                let utc = self.now_time;
+                println!("The date is: {}", utc);
+                // println!("The date is: {}", utc.date());
+
+                // let date = utc.date();
+
+                // let dt = OffsetDateTime::new_in_offset(
+                //     Date::from_calendar_date(2024, Month::December, 15).expect("mumma mia"),
+                //     Time::from_hms_nano(12, 59, 59, 500_000_000).expect("seconds error"),
+                //     UtcOffset::from_hms(-5, 0, 0).expect("timezone error"),
+                // );
+
+                let dt = self.deadline;
+                println!("Calculated date is {}",dt);
+
+                let diff = dt.sub(utc);
+                println!("{}", diff);
+                println!("Weeks until deadline: {}", diff.num_weeks());
+                println!("Days until deadline: {}", diff.num_days());
+                println!("Hours until deadline: {}", diff.num_hours());
+                println!("Seconds until deadline: {}", diff.num_seconds());
+
+                self.countdown = diff.num_days().clone();
+            }
+            //ui.label(format!("Hello '{}', age {}", self.now_time, self.deadline));
+            // if ui.button("Test").clicked(){
+            //
+            //
+            //     let utc = self.now_time;
+            //     println!("The date is: {}", utc);
+            //     // println!("The date is: {}", utc.date());
+            //
+            //     // let date = utc.date();
+            //
+            //     // let dt = OffsetDateTime::new_in_offset(
+            //     //     Date::from_calendar_date(2024, Month::December, 15).expect("mumma mia"),
+            //     //     Time::from_hms_nano(12, 59, 59, 500_000_000).expect("seconds error"),
+            //     //     UtcOffset::from_hms(-5, 0, 0).expect("timezone error"),
+            //     // );
+            //
+            //     let dt = self.deadline;
+            //     println!("Calculated date is {}",dt);
+            //
+            //     let diff = dt.sub(utc);
+            //     println!("{}", diff);
+            //     println!("Weeks until deadline: {}", diff.num_weeks());
+            //     println!("Days until deadline: {}", diff.num_days());
+            //     println!("Hours until deadline: {}", diff.num_hours());
+            //     println!("Seconds until deadline: {}", diff.num_seconds());
+            //
+            //     self.countdown = diff.num_days().clone();
+            //
+            //     println!("Days until deadline minus: {}", diff.num_days());
+            //     let diff_hours = diff.num_hours() -(24*(diff.num_days()));
+            //     println!("Hours until deadline: {}", diff_hours);
+            //     //let diff3 = diff2.sub(diff2.whole_hours());
+            //     let diff_minutes = diff.num_minutes() - (60*diff.num_hours());
+            //     println!("minutes: {}", diff_minutes);
+            //
+            //     let diff_seconds = diff.num_seconds() - (60*diff.num_minutes());
+            //     println!("second until deadline: {}", diff_seconds);
+            // };
+            ui.label(format!("Days to date:{}",self.countdown));
             // ui.image(egui::include_image!(
             //     "../../../crates/egui/assets/ferris.png"
             // ));
