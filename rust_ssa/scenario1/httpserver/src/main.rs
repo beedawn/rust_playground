b03a5e704ce8d600255c9b324a200b2b0788c62a
Bee Schmersal <brandonschmersal@gmail.com>
2024-03-04 17:24:54 -0500

    updates

--- rust_ssa/scenario1/httpserver/src/main.rs
+++ rust_ssa/scenario1/httpserver/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+mod handler;
+mod server;
+mod router;
+user server::Server;
+fn main(){
+//start a server
+let server = Server::new("localhost:3000");
+//run the server
+server.run();
 }
